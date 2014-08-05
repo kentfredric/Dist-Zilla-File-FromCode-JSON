46aaff7a871b0dadc2e316855303633a35f0cbd7
Kent Fredric <kentfredric@gmail.com>
2014-08-05 22:11:27 +1200

    initial commit

---
+++ lib/Dist/Zilla/File/FromCode/JSON.pm
@@ -0,0 +1,19 @@
+use 5.008;    # utf8
+use strict;
+use warnings;
+use utf8;
+
+package Dist::Zilla::File::FromCode::JSON;
+
+our $VERSION = '0.001000';
+
+# ABSTRACT:
+
+# AUTHORITY
+
+use Moose;
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
